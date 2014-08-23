294263c48cc92d3749b4025842e4dcbe75853a2a
Alec Iverson <adiverso93@gmail.com>
2014-08-23 09:00:46 -0700

    Fixed algo to pick the right pixel to color.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <chrono>
 #include <random>
 #include <vector>
-#include <cmath>
 
-#include <SFML/System.hpp>
-#include <SFML/Graphics.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/Image.hpp>
+
+bool done = false;
 
 std::mt19937 generator;
 std::uniform_real_distribution<double> heightDist(0, 1);
@@ -13,7 +14,7 @@ std::uniform_int_distribution<int> startDist(-10000, 10000);
 
 sf::Vector2u size;
 std::vector<double> heights;
-void divideSquare(unsigned x, unsigned y, unsigned w, unsigned h, double c1, double c2, double c3, double c4);
+void divideSquare(bool first, unsigned x, unsigned y, unsigned w, unsigned h, double c1, double c2, double c3, double c4);
 
 int main(int argc, char** argv)
 {
@@ -22,45 +23,42 @@ int main(int argc, char** argv)
 		std::cerr << "Incorrect # of args, must be 2.\n";
 		return 1;
 	}
-	
+
 	size = {static_cast<unsigned>(std::stoi(argv[1])), static_cast<unsigned>(std::stoi(argv[2]))};
-	
 	// resize vector to correct size
-	heights.resize(size.x * size.y);
-	
+	heights.resize(size.x * size.y, 0);
+
 	// seed generator
 	generator.seed(std::chrono::system_clock::now().time_since_epoch().count());
-	
-	// begin the recursive algo!
-	divideSquare(0, 0, size.x, size.y, -1, -1, -1, -1);
-	
+
 	// make the image
 	sf::Image heightMap;
-	heightMap.create(size.x, size.y, {255, 255, 255});
-	
+	heightMap.create(size.x, size.y);
+
+	// begin the recursive algo!
+	divideSquare(true, 0, 0, size.x, size.y, -1, -1, -1, -1);
+
 	// fill the image
 	for(unsigned i = 0; i < size.x * size.y; i++)
 	{
-		double heightVal = heights[i];
-		
-		int r = 255.d * heightVal;
-		int g = 255.d * heightVal;
-		int b = 255.d * heightVal;
-		
+		sf::Uint8 r = 255.0 * heights[i];
+		sf::Uint8 g = 255.0 * heights[i];
+		sf::Uint8 b = 255.0 * heights[i];
+
 		heightMap.setPixel(i % size.x, i / size.y, {r, g, b});
 	}
-	
+
 	heightMap.saveToFile("img" + std::to_string(std::chrono::system_clock::now().time_since_epoch().count()) + ".png");
-	
+
 	return 0;
 }
 
-void divideSquare(unsigned x, unsigned y, unsigned w, unsigned h, double c1, double c2, double c3, double c4)
-{	
+void divideSquare(bool first, unsigned x, unsigned y, unsigned w, unsigned h, double c1, double c2, double c3, double c4)
+{
 	// get corners
 	double topLeft, topRight, botLeft, botRight;
-	
-	if(c1 == -1 && c2 == -1 && c3 == -1 && c4 == -1)
+
+	if((c1 == -1 && c2 == -1 && c3 == -1 && c4 == -1) || first)
 	{
 		topLeft = heightDist(generator);
 		topRight = heightDist(generator);
@@ -74,38 +72,53 @@ void divideSquare(unsigned x, unsigned y, unsigned w, unsigned h, double c1, dou
 		botLeft = c3;
 		botRight = c4;
 	}
-	
+
 	// get midpoints on edges
 	double topEdge, leftEdge, rightEdge, botEdge;
+
+	if(first)
+	{
+		topEdge = heightDist(generator);
+		leftEdge = heightDist(generator);
+		rightEdge = heightDist(generator);
+		botEdge = heightDist(generator);
+	}
+	else
+	{
+		topEdge = (topLeft + topRight) / 2.0;
+		leftEdge = (topLeft + botLeft) / 2.0;
+		rightEdge = (topRight + botRight) / 2.0;
+		botEdge = (botLeft + botRight) / 2.0;
+	}
 	
-	topEdge = (topLeft + topRight) / 2;
-	leftEdge = (topLeft + botLeft) / 2;
-	rightEdge = (topRight + botRight) / 2;
-	botEdge = (botLeft + botRight) / 2;
+	// middle point of the square, add a random amount to keep the map less uniform
+	double middle;
 	
-	// middle point of the square
-	double middle = (topLeft + topRight + botLeft + botRight) / 4;
-	
+	if(first)
+		middle = heightDist(generator);
+	else
+		middle = (topLeft + topRight + botLeft + botRight + heightDist(generator)) / 5.0;
+
 	// assign values
 	// corners
-	heights[x % size.x + y / size.y] = topLeft;
-	heights[x % size.x + (w - 1) + y / size.y] = topRight;
-	heights[x % size.x + y / size.y + (h - 1)] = botLeft;
-	heights[x % size.x + (w - 1) + y / size.y + (h - 1)] = botRight;
-	
+	heights[(x % size.x) + (y % size.y) * size.x] = topLeft;
+	heights[(x + w) % size.x + (y % size.y) * size.x] = topRight;
+	heights[(x % size.x) + ((y + h) % size.y) * size.x] = botLeft;
+	heights[(x + w) % size.x + ((y + h) % size.y) * size.x] = botRight;
+
 	// midpoints
-	heights[x % size.x + ((w - 1) / 2) + y / size.y] = topEdge;
-	heights[x % size.x + y / size.y + ((h - 1) / 2)] = leftEdge;
-	heights[x % size.x + (w - 1) + y / size.y + ((h - 1) / 2)] = rightEdge;
-	heights[x % size.x + ((w - 1) / 2) + y / size.y + (h - 1)] = botEdge;
-	heights[x % size.x + ((w - 1) / 2) + y / size.y + ((h - 1) / 2)] = middle;
-	
+	heights[(x + (w / 2)) % size.x + (y % size.y) * size.x] = topEdge;
+	heights[(x % size.x) + ((y + (h / 2)) % size.y) * size.x] = leftEdge;
+	heights[(x + w) % size.x + ((y + (h / 2)) % size.y) * size.x] = rightEdge;
+	heights[(x + (w / 2)) % size.x + ((y + h) % size.y) * size.x] = botEdge;
+	heights[(x + (w / 2)) % size.x + ((y + (h / 2)) % size.y) * size.x] = middle;
+
 	// if we're too small to continue
-	if(!(w > 2 || h > 2))
+	if(!(w > 1 || h > 1))
 		return;
-	
-	divideSquare(x, y, w / 2, h / 2, topLeft, topEdge, leftEdge, middle);	// top left quad
-	divideSquare(x + w / 2, y, w / 2, h / 2, topEdge, topRight, middle, rightEdge);	// top right quad
-	divideSquare(x, y + h / 2, w / 2, h / 2, leftEdge, middle, botLeft, botEdge);	// bot left quad
-	divideSquare(x + w / 2, y + h / 2, w / 2, h / 2, middle, rightEdge, botEdge, botRight);	// bot right quad
+
+	divideSquare(false, x, y, w / 2.0, h / 2.0, topLeft, topEdge, leftEdge, middle);	// top left quad
+	divideSquare(false, x + w / 2.0, y, w / 2.0, h / 2.0, topEdge, topRight, middle, rightEdge);	// top right quad
+	divideSquare(false, x, y + h / 2.0, w / 2.0, h / 2.0, leftEdge, middle, botLeft, botEdge);	// bot left quad
+	divideSquare(false, x + w / 2.0, y + h / 2.0, w / 2.0, h / 2.0, middle, rightEdge, botEdge, botRight);	// bot right quad
 }
